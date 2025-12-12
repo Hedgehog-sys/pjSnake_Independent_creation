4a5c0496732fc85016a55150ca92d30269570501
Hedgehog-sys <t55606441@gmail.com>
2025-12-12 18:16:53 +0300

    exe_Змейка

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import traceback
 from game import *
 
 if __name__ == "__main__":
-    try:
-        game = Game()
-        game.run()
-    except Exception:
-        with open("error.log", "w", encoding="utf-8") as f:
-            traceback.print_exc(file=f)
-        raise
+    game = Game()
+    game.run()
